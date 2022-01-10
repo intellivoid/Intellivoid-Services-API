94f9502a2bc6de2f97ffbc35b458252f16a2b801
Zi Xing <zixing.narrakas1996@gmail.com>
2022-01-10 11:10:21 -0500

    Added HttpAuthenticationFailure to \Methods\Utilities

---
+++ src/Methods/Utilities/HttpAuthenticationFailure.php
@@ -0,0 +1,49 @@
+<?php
+
+    namespace Methods\Utilities;
+
+    use Exception;
+
+    /**
+     * Class apiAuthenticationFailure
+     */
+    class HttpAuthenticationFailure extends Exception
+    {
+        /**
+         * @var int
+         */
+        private int $coa_error_code;
+
+        /**
+         * @var int
+         */
+        private int $status_code;
+
+        /**
+         * HttpAuthenticationFailure constructor.
+         * @param int $coa_error_code
+         * @param int $status_code
+         */
+        public function __construct(int $coa_error_code, int $status_code)
+        {
+            parent::__construct(resolve_error_code($coa_error_code), $coa_error_code);
+            $this->coa_error_code = $coa_error_code;
+            $this->status_code = $status_code;
+        }
+
+        /**
+         * @return int
+         */
+        public function getCoaErrorCode(): int
+        {
+            return $this->coa_error_code;
+        }
+
+        /**
+         * @return int
+         */
+        public function getStatusCode(): int
+        {
+            return $this->status_code;
+        }
+    }
